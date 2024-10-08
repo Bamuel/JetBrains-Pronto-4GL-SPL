9e89f32f3867ee853a002084e9f0674bb8426dba
Samuel Levin <me@bamuel.com>
2024-10-08 12:42:42 +1100

    cleanup

--- src/main/java/com/bamuel/spllang/SPLSyntaxHighlighter.java
+++ src/main/java/com/bamuel/spllang/SPLSyntaxHighlighter.java
@@ -14,39 +14,20 @@ import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAtt
 
 public class SPLSyntaxHighlighter extends SyntaxHighlighterBase {
 
-    public static final TextAttributesKey SEPARATOR =
-            createTextAttributesKey("SPL_SEPARATOR", DefaultLanguageHighlighterColors.OPERATION_SIGN);
-    public static final TextAttributesKey KEY =
-            createTextAttributesKey("SPL_KEY", DefaultLanguageHighlighterColors.KEYWORD);
-    public static final TextAttributesKey KEYWORD =
-            createTextAttributesKey("SPL_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
-    public static final TextAttributesKey FUNCTION_DECLARATION =
-            createTextAttributesKey("SPL_FUNCTION_DECLARATION", DefaultLanguageHighlighterColors.FUNCTION_DECLARATION);
-    public static final TextAttributesKey VALUE =
-            createTextAttributesKey("SPL_VALUE", DefaultLanguageHighlighterColors.STRING);
-    public static final TextAttributesKey STRING =
-            createTextAttributesKey("SPL_STRING", DefaultLanguageHighlighterColors.STRING);
-    public static final TextAttributesKey NUMBER =
-            createTextAttributesKey("SPL_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
-    public static final TextAttributesKey COMMA =
-            createTextAttributesKey("SPL_COMMA", DefaultLanguageHighlighterColors.COMMA);
-    public static final TextAttributesKey LPARENTHESES =
-            createTextAttributesKey("LPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
-    public static final TextAttributesKey RPARENTHESES =
-            createTextAttributesKey("RPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
-    public static final TextAttributesKey COMMENT =
-            createTextAttributesKey("SPL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
-    public static final TextAttributesKey BLOCK_COMMENT =
-            createTextAttributesKey("SPL_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
-    public static final TextAttributesKey BAD_CHARACTER =
-            createTextAttributesKey("SPL_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
+    public static final TextAttributesKey KEYWORD = createTextAttributesKey("SPL_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
+    public static final TextAttributesKey FUNCTION_DECLARATION = createTextAttributesKey("SPL_FUNCTION_DECLARATION", DefaultLanguageHighlighterColors.FUNCTION_DECLARATION);
+    public static final TextAttributesKey STRING = createTextAttributesKey("SPL_STRING", DefaultLanguageHighlighterColors.STRING);
+    public static final TextAttributesKey NUMBER = createTextAttributesKey("SPL_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
+    public static final TextAttributesKey COMMA = createTextAttributesKey("SPL_COMMA", DefaultLanguageHighlighterColors.COMMA);
+    public static final TextAttributesKey LPARENTHESES = createTextAttributesKey("LPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
+    public static final TextAttributesKey RPARENTHESES = createTextAttributesKey("RPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
+    public static final TextAttributesKey COMMENT = createTextAttributesKey("SPL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
+    public static final TextAttributesKey BLOCK_COMMENT = createTextAttributesKey("SPL_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
+    public static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey("SPL_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
     private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-    private static final TextAttributesKey[] SEPARATOR_KEYS = new TextAttributesKey[]{SEPARATOR};
-    private static final TextAttributesKey[] KEY_KEYS = new TextAttributesKey[]{KEY};
     private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
     private static final TextAttributesKey[] FUNCTION_DECLARATION_KEYS = new TextAttributesKey[]{FUNCTION_DECLARATION};
-    private static final TextAttributesKey[] VALUE_KEYS = new TextAttributesKey[]{VALUE};
     private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
     private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
     private static final TextAttributesKey[] COMMA_KEYS = new TextAttributesKey[]{COMMA};
@@ -63,20 +44,11 @@ public class SPLSyntaxHighlighter extends SyntaxHighlighterBase {
 
     @Override
     public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
-        if (tokenType.equals(SPLTypes.SEPARATOR)) {
-            return SEPARATOR_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.KEY)) {
-            return KEY_KEYS;
-        }
         if (tokenType.equals(SPLTypes.KEYWORD)) {
             return KEYWORD_KEYS;
         }
         if (tokenType.equals(SPLTypes.FUNCTION_DECLARATION)) {
             return FUNCTION_DECLARATION_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.VALUE)) {
-            return VALUE_KEYS;
         }
         if (tokenType.equals(SPLTypes.STRING)) {
             return STRING_KEYS;
--- src/main/java/com/bamuel/spllang/psi/SPLTokenSets.java
+++ src/main/java/com/bamuel/spllang/psi/SPLTokenSets.java
@@ -4,8 +4,5 @@ import com.intellij.psi.tree.TokenSet;
 
 public interface SPLTokenSets {
 
-    TokenSet IDENTIFIERS = TokenSet.create(SPLTypes.KEY);
-
     TokenSet COMMENTS = TokenSet.create(SPLTypes.COMMENT);
-
 }
