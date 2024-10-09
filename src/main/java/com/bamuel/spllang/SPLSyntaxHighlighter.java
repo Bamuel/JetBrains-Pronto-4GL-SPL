99da42b96b48b699f6cbb3a24917b08d1ee3c613
Samuel Levin <me@bamuel.com>
2024-10-09 12:29:49 +1100

    spl highlighter update

--- src/main/java/com/bamuel/spllang/SPLSyntaxHighlighter.java
+++ src/main/java/com/bamuel/spllang/SPLSyntaxHighlighter.java
@@ -10,6 +10,9 @@ import com.intellij.psi.tree.IElementType;
 import com.bamuel.spllang.psi.SPLTypes;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static com.intellij.openapi.editor.colors.TextAttributesKey.createTextAttributesKey;
 
 public class SPLSyntaxHighlighter extends SyntaxHighlighterBase {
@@ -19,22 +22,26 @@ public class SPLSyntaxHighlighter extends SyntaxHighlighterBase {
     public static final TextAttributesKey STRING = createTextAttributesKey("SPL_STRING", DefaultLanguageHighlighterColors.STRING);
     public static final TextAttributesKey NUMBER = createTextAttributesKey("SPL_NUMBER", DefaultLanguageHighlighterColors.NUMBER);
     public static final TextAttributesKey COMMA = createTextAttributesKey("SPL_COMMA", DefaultLanguageHighlighterColors.COMMA);
-    public static final TextAttributesKey LPARENTHESES = createTextAttributesKey("LPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
-    public static final TextAttributesKey RPARENTHESES = createTextAttributesKey("RPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
+    public static final TextAttributesKey LPARENTHESES = createTextAttributesKey("SPL_LPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
+    public static final TextAttributesKey RPARENTHESES = createTextAttributesKey("SPL_RPAREN", DefaultLanguageHighlighterColors.PARENTHESES);
     public static final TextAttributesKey COMMENT = createTextAttributesKey("SPL_COMMENT", DefaultLanguageHighlighterColors.LINE_COMMENT);
     public static final TextAttributesKey BLOCK_COMMENT = createTextAttributesKey("SPL_BLOCK_COMMENT", DefaultLanguageHighlighterColors.BLOCK_COMMENT);
     public static final TextAttributesKey BAD_CHARACTER = createTextAttributesKey("SPL_BAD_CHARACTER", HighlighterColors.BAD_CHARACTER);
 
-    private static final TextAttributesKey[] BAD_CHAR_KEYS = new TextAttributesKey[]{BAD_CHARACTER};
-    private static final TextAttributesKey[] KEYWORD_KEYS = new TextAttributesKey[]{KEYWORD};
-    private static final TextAttributesKey[] FUNCTION_DECLARATION_KEYS = new TextAttributesKey[]{FUNCTION_DECLARATION};
-    private static final TextAttributesKey[] STRING_KEYS = new TextAttributesKey[]{STRING};
-    private static final TextAttributesKey[] NUMBER_KEYS = new TextAttributesKey[]{NUMBER};
-    private static final TextAttributesKey[] COMMA_KEYS = new TextAttributesKey[]{COMMA};
-    private static final TextAttributesKey[] PARENTHESES_KEYS = new TextAttributesKey[]{LPARENTHESES, RPARENTHESES};
-    private static final TextAttributesKey[] COMMENT_KEYS = new TextAttributesKey[]{COMMENT};
-    private static final TextAttributesKey[] BLOCK_COMMENT_KEYS = new TextAttributesKey[]{BLOCK_COMMENT};
-    private static final TextAttributesKey[] EMPTY_KEYS = new TextAttributesKey[0];
+    private static final Map<IElementType, TextAttributesKey[]> TOKEN_HIGHLIGHT_MAP = new HashMap<>();
+
+    static {
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.KEYWORD, new TextAttributesKey[]{KEYWORD});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.FUNCTION_DECLARATION, new TextAttributesKey[]{FUNCTION_DECLARATION});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.STRING, new TextAttributesKey[]{STRING});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.NUMBER, new TextAttributesKey[]{NUMBER});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.COMMA, new TextAttributesKey[]{COMMA});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.LPAREN, new TextAttributesKey[]{LPARENTHESES});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.RPAREN, new TextAttributesKey[]{RPARENTHESES});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.COMMENT, new TextAttributesKey[]{COMMENT});
+        TOKEN_HIGHLIGHT_MAP.put(SPLTypes.BLOCK_COMMENT, new TextAttributesKey[]{BLOCK_COMMENT});
+        TOKEN_HIGHLIGHT_MAP.put(TokenType.BAD_CHARACTER, new TextAttributesKey[]{BAD_CHARACTER});
+    }
 
     @NotNull
     @Override
@@ -44,33 +51,6 @@ public class SPLSyntaxHighlighter extends SyntaxHighlighterBase {
 
     @Override
     public TextAttributesKey @NotNull [] getTokenHighlights(IElementType tokenType) {
-        if (tokenType.equals(SPLTypes.KEYWORD)) {
-            return KEYWORD_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.FUNCTION_DECLARATION)) {
-            return FUNCTION_DECLARATION_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.STRING)) {
-            return STRING_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.NUMBER)) {
-            return NUMBER_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.COMMA)) {
-            return COMMA_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.LPAREN) || tokenType.equals(SPLTypes.RPAREN)) {
-            return PARENTHESES_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.COMMENT)) {
-            return COMMENT_KEYS;
-        }
-        if (tokenType.equals(SPLTypes.BLOCK_COMMENT)) {
-            return BLOCK_COMMENT_KEYS;
-        }
-        if (tokenType.equals(TokenType.BAD_CHARACTER)) {
-            return BAD_CHAR_KEYS;
-        }
-        return EMPTY_KEYS;
+        return TOKEN_HIGHLIGHT_MAP.getOrDefault(tokenType, new TextAttributesKey[0]);
     }
 }
