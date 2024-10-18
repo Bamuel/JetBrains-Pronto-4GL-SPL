f69842ad9ec5bf2726b45a8a663eb753ba6055e6
Samuel Levin <me@bamuel.com>
2024-10-18 11:13:00 +1100

    Fixed Deprecated method TextAttributesKey.createTextAttributesKey(...)

--- src/main/java/com/bamuel/spllang/ERRSyntaxHighlighter.java
+++ src/main/java/com/bamuel/spllang/ERRSyntaxHighlighter.java
@@ -3,31 +3,22 @@ package com.bamuel.spllang;
 import com.bamuel.spllang.psi.ERRTypes;
 import com.intellij.lexer.Lexer;
 import com.intellij.openapi.editor.DefaultLanguageHighlighterColors;
-import com.intellij.openapi.editor.markup.TextAttributes;
 import com.intellij.openapi.editor.colors.TextAttributesKey;
 import com.intellij.openapi.fileTypes.SyntaxHighlighterBase;
 import com.intellij.psi.tree.IElementType;
-import com.intellij.ui.JBColor;
 import org.jetbrains.annotations.NotNull;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ERRSyntaxHighlighter extends SyntaxHighlighterBase {
 
     // Custom attributes for ERROR, WARNING, and INFO with specific colors and bold fonts
-    public static final TextAttributesKey ERROR = TextAttributesKey.createTextAttributesKey(
-            "LOG_ERROR", new TextAttributes(new JBColor(new Color(218, 53, 68), new Color(218, 53, 68)), null, null, null, Font.BOLD)
-    );
+    public static final TextAttributesKey ERROR = TextAttributesKey.createTextAttributesKey("LOG_ERROR", DefaultLanguageHighlighterColors.KEYWORD);
 
-    public static final TextAttributesKey WARNING = TextAttributesKey.createTextAttributesKey(
-            "LOG_WARNING", new TextAttributes(new JBColor(new Color(253, 191, 7), new Color(253, 191, 7)), null, null, null, Font.BOLD)
-    );
+    public static final TextAttributesKey WARNING = TextAttributesKey.createTextAttributesKey("LOG_WARNING", DefaultLanguageHighlighterColors.KEYWORD);
 
-    public static final TextAttributesKey INFO = TextAttributesKey.createTextAttributesKey(
-            "LOG_INFO", new TextAttributes(new JBColor(new Color(23, 161, 183), new Color(23, 161, 183)), null, null, null, Font.BOLD)
-    );
+    public static final TextAttributesKey INFO = TextAttributesKey.createTextAttributesKey("LOG_INFO", DefaultLanguageHighlighterColors.KEYWORD);
 
     public static final TextAttributesKey KEYWORD = TextAttributesKey.createTextAttributesKey("LOG_KEYWORD", DefaultLanguageHighlighterColors.KEYWORD);
     public static final TextAttributesKey FILEPATH = TextAttributesKey.createTextAttributesKey("LOG_FILEPATH", DefaultLanguageHighlighterColors.NUMBER);
